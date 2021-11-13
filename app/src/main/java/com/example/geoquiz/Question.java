449944a5485d1839d5777a703d5bb2f139e650f5
Rudy <rudy.walling@gmail.com>
2021-11-13 13:55:05 -0500

    Finished part A

---
+++ app/src/main/java/com/example/geoquiz/Question.java
@@ -0,0 +1,19 @@
+package com.example.geoquiz;
+
+public class Question {
+    int textResId;
+    boolean answer;
+
+    public int getTextResId() {
+        return textResId;
+    }
+
+    public void setTextResId(int textResId) {
+        this.textResId = textResId;
+    }
+
+    Question(int textResId, boolean answer){
+        this.textResId=textResId;
+        this.answer=answer;
+    }
+}
